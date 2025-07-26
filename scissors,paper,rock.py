4844c3713fcfdc2ebda60610aa298e60cbae9da5
AAI <amirabbasrostmi1388.29@gmail.com>
2025-07-26 18:11:37 +0330

    Add files via upload

---
+++ scissors,paper,rock.py
@@ -0,0 +1,65 @@
+import random
+
+game=['stone','paper','scissors']
+ 
+computer=''
+
+you_point=0
+computer_point=0 
+
+loop_game=True
+
+
+while(loop_game):
+
+
+
+    you = input("select!(paper,stone,scissors) ")
+
+    if you=="0":
+        shart_bazi=False
+
+
+    rand=random.randint(0,2)
+    computer=game[rand]
+
+    if you=="stone" and computer=='paper':
+        computer_point=computer_point+1
+
+    if you=='scissors' and computer=='paper':
+        you_point=you_point+1
+
+
+    if you=='stone' and computer=='scissors':
+        you_point=you_point+1
+
+    if you=='paper' and computer=='scissors':
+        computer_point=computer_point+1
+
+
+    if you=='scissors' and computer=='stone':
+        computer_point=computer_point+1
+
+    if you=='paper' and computer=='stone':
+        you_point=you_point+1
+
+
+
+    print("YOU: (", you_point  ,")",   you,"     COM:(", computer_point  ,")",computer)
+
+    if you_point>=3 or computer_point>=3:
+        loop_game=False
+
+
+
+
+if you_point>computer_point:
+    print("")
+    print("YOU WIN!")
+else:
+    print("")
+    print("YOU LOSE :(")
+
+print("YOU: ",you_point)
+print("COM: ",computer_point)
+
